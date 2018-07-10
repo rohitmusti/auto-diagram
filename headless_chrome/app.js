440d206a4629e84b646d967b5567b88feedb59f2
Benjamin Kincaid <bkin18@bu.edu>
2018-07-10 16:44:55 -0400

    png generation and png fetch working synchronously

--- headless_chrome/app.js
+++ headless_chrome/app.js
@@ -1,11 +1,14 @@
-var screenshot = require('./screenshot.js')
+var takeScreenShot = require('./screenshot.js')
 var express = require('express')
 var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  screenshot();
-  res.sendFile(__dirname + '/output.jpeg')
+      takeScreenShot()
+      .then(() => {
+        res.sendFile(__dirname + '/output.jpeg')
+      })
+      .catch(() => console.log("error"));
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- headless_chrome/screenshot.js
+++ headless_chrome/screenshot.js
@@ -8,53 +8,58 @@ const viewportWidth = 800;
 
 // Start the Chrome Debugging Protocol
 module.exports = function takeScreenShot(){
-  CDP(async function(client) {
-    // Extract used DevTools domains.
-    const {DOM, Emulation, Network, Page} = client;
+  return new Promise(function(resolve, reject){
+    CDP(async function(client) {
+      // Extract used DevTools domains.
+      const {DOM, Emulation, Network, Page} = client;
 
-    // Enable events on domains we are interested in.
-    await Page.enable();
-    await DOM.enable();
-    await Network.enable();
+      // Enable events on domains we are interested in.
+      await Page.enable();
+      await DOM.enable();
+      await Network.enable();
 
-    await Page.navigate({url});
+      await Page.navigate({url});
 
-    Page.loadEventFired(async () => {
-      // measure the height of the rendered page and use Emulation.setVisibleSize
-      const {root: {nodeId: documentNodeId}} = await DOM.getDocument();
-      const {nodeId: bodyNodeId} = await DOM.querySelector({
-        selector: 'body',
-        nodeId: documentNodeId,
+      Page.loadEventFired(async () => {
+        // measure the height of the rendered page and use Emulation.setVisibleSize
+        const {root: {nodeId: documentNodeId}} = await DOM.getDocument();
+        const {nodeId: bodyNodeId} = await DOM.querySelector({
+          selector: 'body',
+          nodeId: documentNodeId,
+        });
+        const {model: {height}} = await DOM.getBoxModel({nodeId: bodyNodeId});
+
+        console.log("Set visible size to the height of the dom", height);
+
+        const deviceMetrics = {
+          width: viewportWidth,
+          height: height,
+          deviceScaleFactor: 1,
+          mobile: false,
+          fitWindow: false,
+        };
+        await Emulation.setDeviceMetricsOverride(deviceMetrics);
+        await Emulation.setVisibleSize({width: viewportWidth, height: height});
+
+        // get the base64 screenshot.
+        const screenshot = await Page.captureScreenshot({format});
+
+        // Save the base64 screenshot to binary image file
+        const buffer = new Buffer(screenshot.data, 'base64');
+        file.writeFile('output.jpeg', buffer, 'base64', function(err) {
+          if (err) {
+            console.error(err);
+            reject();
+          } else {
+            console.log('Screenshot saved');
+            resolve();
+          }
+          client.close();
+        });
       });
-      const {model: {height}} = await DOM.getBoxModel({nodeId: bodyNodeId});
-
-      console.log("Set visible size to the height of the dom", height);
-
-      const deviceMetrics = {
-        width: viewportWidth,
-        height: height,
-        deviceScaleFactor: 1,
-        mobile: false,
-        fitWindow: false,
-      };
-      await Emulation.setDeviceMetricsOverride(deviceMetrics);
-      await Emulation.setVisibleSize({width: viewportWidth, height: height});
-
-      // get the base64 screenshot.
-      const screenshot = await Page.captureScreenshot({format});
-
-      // Save the base64 screenshot to binary image file
-      const buffer = new Buffer(screenshot.data, 'base64');
-      file.writeFile('output.jpeg', buffer, 'base64', function(err) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Screenshot saved');
-        }
-        client.close();
-      });
+    }).on('error', err => {
+      console.error('Cannot connect to browser:', err);
+      reject();
     });
-  }).on('error', err => {
-    console.error('Cannot connect to browser:', err);
   });
 };
