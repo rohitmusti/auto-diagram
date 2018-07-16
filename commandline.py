190f6e6fc18d00de0c7f2e86049da9c817a3774e
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-16 14:42:51 -0400

    made it so that one script does everything

--- commandline.py
+++ commandline.py
@@ -8,12 +8,16 @@ def copying():
     original = './public/example-api.json'
     copyfile(file, original)
     if (browser == 1):
-    	headlessCMD += "chromium --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 & "
+        headlessCMD += "chromium --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 & "
+        os.system('npm run serve &')
+        os.system(headlessCMD)
+        os.system('sleep 10 && node ./headless_chrome/screenshot.js && mv ./headless_chrome/output.jpeg ./output.jpeg && killall npm && killall node && killall chromium && echo "done"')
     elif (browser == 2):
-    	headlessCMD += "google-chrome --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 & "
-    os.system('npm run serve &')
-    os.system(headlessCMD)
-    os.system('sleep 5 && node ./headless_chrome/screenshot.js &')
+        headlessCMD += "google-chrome --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 & "
+        os.system('npm run serve &')
+        os.system(headlessCMD)
+        os.system('sleep 10 && node ./headless_chrome/screenshot.js && mv ./headless_chrome/output.jpeg ./output.jpeg && killall npm && killall node && killall google-chrome && echo "done"')
+
 
 
 copying()
